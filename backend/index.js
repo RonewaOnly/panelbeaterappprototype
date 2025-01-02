ceeb198a3984e7cf660a056a45f6d6b5d9c92169
ronewa <ronewamaselesele200@gmail.com>
2025-01-02 20:24:21 +0200

    `feat: updated backend/index.js and package.json with additional imports and middleware`

--- backend/index.js
+++ backend/index.js
@@ -1,10 +1,20 @@
 import e from "express";
-const app = e();    
+import path from "path"; 
+import fs from "fs";
+import bodyParser from "body-parser";
+const app = e(); 
+
+app.use(e.json());
+//I want the app to read documents and images from the public folder
+app.use(e.static('public'));
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.json({ limit: '10mb' }));
 
 app.get('/', (req, res) => {
     res.send('Hello World!');
 }
 );
+
 
 app.listen(3000, () => {
     console.log('Server is running on port 3000');

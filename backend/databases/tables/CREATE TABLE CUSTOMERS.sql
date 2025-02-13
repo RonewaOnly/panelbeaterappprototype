a95fd328c52282ca5d86857cca87875e93af9b13
ronewa <ronewamaselesele200@gmail.com>
2025-02-13 17:02:44 +0200

    feat: create CUSTOMERS table with relevant fields for customer data management

---
+++ backend/databases/tables/CREATE TABLE CUSTOMERS.sql
@@ -0,0 +1,12 @@
+CREATE TABLE CUSTOMERS(
+    CUSTOMER_ID INT PRIMARY KEY ,
+    CUSTOMER_NAME VARCHAR(255) NOT NULL,
+    CUSTOMER_ADDRESS VARCHAR(255) NOT NULL,
+    CUSTOMER_PHONE VARCHAR(255) NOT NULL,
+    CAR_MODEL VARCHAR(255) NOT NULL,
+    CAR_YEAR INT NOT NULL,
+    CAR_COLOR VARCHAR(255),
+    CAR_PROBLEM_DESCRIPTION VARCHAR(255) NOT NULL,
+    CAR_REPAIR_STATUS VARCHAR(255)  DEFAULT 'NOT VIEWED' NOT NULL 
+);
+--when working with default values, it is important to note that the default value is only applied to new rows that are added to the table. It does not affect existing rows. And also that the 'Not Null' must be after the default value.
